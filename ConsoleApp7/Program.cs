4a31a4fc8ade7c9b1b292fda02232bd9862befc4
kcarpentgh <kcarpenter@xactware.com>
2017-04-16 17:24:06 -0600

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string stringinput = Console.ReadLine();
+            int numberinput = Convert.ToInt32(stringinput);
+            bool result = (numberinput % 2 == 0);
+
+            if (result == true)
+                Console.WriteLine("This is an even number.");
+            else
+                Console.WriteLine("This is an odd number.");
+
+            Console.ReadKey();
+
+        }
+    }
+}
